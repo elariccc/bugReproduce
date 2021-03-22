99560493ef93c490719da492947667a6f355cb9a
Eldar Dzhunushaliev <eldar.junushaliev@tucana.aero>
2021-03-22 21:36:54 +0300

    init

---
+++ ExcelWebAddIn2Web/Home.js
@@ -0,0 +1,115 @@
+﻿(function () {
+    "use strict";
+
+    var cellToHighlight;
+    var messageBanner;
+
+    // The initialize function must be run each time a new page is loaded.
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            // Initialize the notification mechanism and hide it
+            var element = document.querySelector('.MessageBanner');
+            messageBanner = new components.MessageBanner(element);
+            messageBanner.hideBanner();
+            
+            // If not using Excel 2016, use fallback logic.
+            if (!Office.context.requirements.isSetSupported('ExcelApi', '1.1')) {
+                $("#template-description").text("This sample will display the value of the cells that you have selected in the spreadsheet.");
+                $('#button-text').text("Display!");
+                $('#button-desc').text("Display the selection");
+
+                $('#highlight-button').click(displaySelectedCells);
+                return;
+            }
+
+            $("#template-description").text("This sample highlights the highest value from the cells you have selected in the spreadsheet.");
+            $('#button-text').text("Highlight!");
+            $('#button-desc').text("Highlights the largest number.");
+                
+            loadSampleData();
+
+            // Add a click event handler for the highlight button.
+            $('#highlight-button').click(hightlightHighestValue);
+        });
+    };
+
+    function loadSampleData() {
+        var values = [
+            [Math.floor(Math.random() * 1000), Math.floor(Math.random() * 1000), Math.floor(Math.random() * 1000)],
+            [Math.floor(Math.random() * 1000), Math.floor(Math.random() * 1000), Math.floor(Math.random() * 1000)],
+            [Math.floor(Math.random() * 1000), Math.floor(Math.random() * 1000), Math.floor(Math.random() * 1000)]
+        ];
+
+        // Run a batch operation against the Excel object model
+        Excel.run(function (ctx) {
+            // Create a proxy object for the active sheet
+            var sheet = ctx.workbook.worksheets.getActiveWorksheet();
+            // Queue a command to write the sample data to the worksheet
+            sheet.getRange("B3:D5").values = values;
+
+            // Run the queued-up commands, and return a promise to indicate task completion
+            return ctx.sync();
+        })
+        .catch(errorHandler);
+    }
+
+    function hightlightHighestValue() {
+      Excel.run(async context => {
+        const
+          worksheet = context.workbook.worksheets.getActiveWorksheet(),
+          activeCell = context.workbook.getActiveCell(),
+          charts = worksheet.charts,
+          cell = worksheet.getRange('B3');
+
+        const someChart = charts.add('XYScatterLines', activeCell);
+        const chartSeries = [];
+
+        const xAxis = someChart.axes.categoryAxis;
+
+        xAxis.scaleType = 'Logarithmic';
+        xAxis.logBase = 10;
+
+        xAxis.maximum = 1000.5;
+        //xAxis.minimum = 0.9;
+
+        for (let i = 0; i < 3; i++) {
+          chartSeries.push(someChart.series.add(String(i)));
+
+          chartSeries[i].setXAxisValues(cell.getAbsoluteResizedRange(1, 3));
+          chartSeries[i].setValues(cell.getCell(i, 0).getAbsoluteResizedRange(1, 3));
+
+          //chartSeries[i].markerStyle = 'None';
+        }
+      })
+        .catch(errorHandler);
+    }
+
+    function displaySelectedCells() {
+        Office.context.document.getSelectedDataAsync(Office.CoercionType.Text,
+            function (result) {
+                if (result.status === Office.AsyncResultStatus.Succeeded) {
+                    showNotification('The selected text is:', '"' + result.value + '"');
+                } else {
+                    showNotification('Error', result.error.message);
+                }
+            });
+    }
+
+    // Helper function for treating errors
+    function errorHandler(error) {
+        // Always be sure to catch any accumulated errors that bubble up from the Excel.run execution
+        showNotification("Error", error);
+        console.log("Error: " + error);
+        if (error instanceof OfficeExtension.Error) {
+            console.log("Debug info: " + JSON.stringify(error.debugInfo));
+        }
+    }
+
+    // Helper function for displaying notifications
+    function showNotification(header, content) {
+        $("#notification-header").text(header);
+        $("#notification-body").text(content);
+        messageBanner.showBanner();
+        messageBanner.toggleExpansion();
+    }
+})();
